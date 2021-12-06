dcf252d0137117be2f5c6998de5d0da9db18047e
Alan Horne <alan.horne@gmail.com>
2021-12-06 00:49:41 -0600

    Added some headers to the output

--- AdventOfCode2021/Program.fs
+++ AdventOfCode2021/Program.fs
@@ -1,14 +1,23 @@
 namespace AdventOfCode2021
 
 module Program =
-    
+
+    let header (day : int) (part : int) =
+        printfn ""
+        printfn $"Day {day}, Part {part}"
+        
+        
     [<EntryPoint>]
     let main _argv =
 
+        header 1 1
         Day1.part1 ()
+        header 1 2
         Day1.part2 ()
 
+        header 2 1
         Day2.part1 ()
+        header 2 2
         Day2.part2 ()
-
+        
         0
