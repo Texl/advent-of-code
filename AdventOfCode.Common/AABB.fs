9b6b0389547d5ae7dfd1bc8a708bb8ecea1eeded
Alan Horne <alan.horne@gmail.com>
2023-12-03 01:55:04 -0600

    Day 03

---
+++ AdventOfCode.Common/AABB.fs
@@ -0,0 +1,18 @@
+﻿namespace AdventOfCode.Common
+
+[<AutoOpen>]
+module AABBTypes =
+   type AABB =
+        { Min : Vector2
+          Max : Vector2 }
+      with
+      member self.Contains (p : Vector2) =
+         p.R >= self.Min.R &&
+         p.R <= self.Max.R &&
+         p.C >= self.Min.C &&
+         p.C <= self.Max.C
+
+[<RequireQualifiedAccess>]
+module AABB =
+   let contains (p : Vector2) (aabb : AABB) =
+      aabb.Contains p
